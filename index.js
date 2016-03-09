831cd9ad1fc10fc23d5b250d312ba40b23418e43
Daniel Ilkovich <daniel+github@betaworks.com>
2016-03-09 15:31:13 -0500

    initial

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var _       = require('lodash')
+  , google  = require('googleapis')
+  , q       = require('q')
+  , service = google.script('v1')
+;
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +12,49 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var OAuth2       = google.auth.OAuth2
+          , oauth2Client = new OAuth2()
+          , access_token = dexter.provider('google').credentials('access_token')
+          , self         = this
+          , promises     = []
+          , scriptId     = step.input('script_id').first()
+          , fn           = step.input('function').first()
+          , parameters   = step.input('parameters').toArray()
+          , devMode      = step.input('dev_mode').first()
+          , data
+        ;
+
+        // set credentials
+        oauth2Client.setCredentials({ access_token: access_token });
+
+        google.options({ auth: oauth2Client });
+
+        data = {
+            resource: {
+                function     : fn
+                , parameters : parameters //[ step.input('spreadsheet_id').first(), 'hello' ]
+                , devMode    : _.isNil(devMode) ? false : devMode // true
+            },
+            scriptId: scriptId
+        };
+
+        promisify(service.scripts.run.bind(service.scripts), data, 'response')
+          .then(this.complete.bind(this))
+          .catch(function(err) { self.fail(require('util').inspect(err, { depth: 5 })); })
+        ;
     }
 };
+
+
+function promisify(fn, data, path) {
+    var deferred = q.defer(); 
+
+    fn(data, function(err, result) {
+        return err
+          ? deferred.reject(err)
+          : deferred.resolve(path ? _.get(result, path) : result)
+        ;
+    });
+
+    return deferred.promise;
+}
